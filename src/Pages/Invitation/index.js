3d587ded669754b7ae3fc634c28415e1118712f0
Rado Poghossian <rado.poghossian@padcllc.com>
2022-11-30 22:04:22 +0400

    fixed bug

--- src/Pages/Invitation/index.js
+++ src/Pages/Invitation/index.js
@@ -1,7 +1,15 @@
+/* eslint-disable jsx-a11y/alt-text */
 import {React} from 'react';
+
+import style from './Invitation.module.css';
+
+import image1 from '../../Assets/Images/image1.jpg';
 
 export function Invitation(){
     return(
-        <>Invitation</>
+        <>
+        <div className={style.Invitation}></div>
+        <img src={image1} className={style.header_img}/>
+        </>
     )
 }
