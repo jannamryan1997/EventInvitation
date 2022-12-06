04bd3fd0d7a4b39fbc62f32523c62f8a861d0216
Janna Mryan <jannamryan8@gmail.com>
2022-12-06 16:19:01 +0400

    created home page design

--- src/Pages/Home/index.js
+++ src/Pages/Home/index.js
@@ -1,10 +1,51 @@
 import { React } from 'react';
 
+import { Carousel } from 'react-responsive-carousel';
+
+
+import weeding1 from '../../Assets/Images/weeding1.jpg';
+import weeding2 from '../../Assets/Images/weeding2.png';
+import weeding3 from '../../Assets/Images/weeding3.jpg';
+import weeding4 from '../../Assets/Images/weeding4.jpg';
+
+import homebackground from '../../Assets/Images/home_background.png';
+
 export function Home() {
 
     return (
-        <>home
-        <h1 className="animate__animated animate__fadeInDown">An animated element</h1>
+        <>
+            <div className='home__main'>
+                <div className='home__container'>
+                    <img src={homebackground} className="home__background" />
+                    <div className='carousel__content'>
+                    <Carousel
+                        autoPlay={true}
+                        infiniteLoop={true}
+                        showArrows={true}
+                        showThumbs={false}
+                    >
+                        <div>
+                            <img src={weeding1} className='wedding_img' />
+                        </div>
+                        <div>
+                            <img src={weeding2} className='wedding_img' />
+                        </div>
+                        <div>
+                            <img src={weeding3} className='wedding_img' />
+                        </div>
+                        <div>
+                            <img src={weeding4} className='wedding_img' />
+                        </div>
+                    </Carousel>
+                    </div>
+                </div>
+
+            </div>
         </>
     )
 }
+
+
+{/* <span className='home__main__description'>Վիրտուալ հրավիրատոմսեր <br/> ցանկացած առիթի համար</span> */ }
+
+
